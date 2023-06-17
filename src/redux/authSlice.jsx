b527aca6bee6bc2fc8800d3fdf59929a35b7fa70
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-17 23:21:16 +0300

    set up authslice and redux store

---
+++ src/redux/authSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const signUpRequest = post 
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    token: null,
+  },
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    clearToken: (state) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setToken, clearToken } = authSlice.actions;
+
+export default authSlice.reducer;
