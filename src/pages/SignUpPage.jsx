b5fde5d5663a784b69ad022f73a709d99cdcb4b7
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-17 23:58:47 +0300

    Complete sign up page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,70 @@
-function SignUpPage() {
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const encryptToken = (token) => {
+    const encryptedToken = CryptoJS.AES.encrypt(token, 'secret-key').toString();
+    return encryptedToken;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://booking-api-nhmg.onrender.com/users/signup', {
+        username,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+      });
+      
+      const { token } = response.data;
+      const encryptedToken = encryptToken(token);
+      sessionStorage.setItem('token', encryptedToken);
+
+      window.location.replace('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>SignUpPage</h1>
-    </div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input 
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input 
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+    </form>
+  </div>
   );
-}
+};
 
-export default SignUpPage;
+export default SignUp;
