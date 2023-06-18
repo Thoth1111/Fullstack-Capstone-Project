56257928975d89a443e50290fb9cf3789af5b963
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-18 08:33:05 +0300

    Add dummy home & refactor for notifications

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import { encryptToken } from '../helpers/encryption';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 const Login = () => {
+  const location = useLocation();
+  const { state } = location;
+  const success = state?.success;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,31 +25,40 @@ const Login = () => {
       const encryptedToken = encryptToken(token);
       sessionStorage.setItem('token', encryptedToken);
 
-      window.location.replace('/');
+      navigate('/dummyHome', { state: { success: 'loggedin' } })
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div>
-      <h2>Log In</h2>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log In</button>
-      </form>
-    </div>
+    <>
+      <div>
+        {success === 'signedup' && (
+          <div className="notification">
+            Signed up successfully
+          </div>
+        )}
+      </div>
+      <div>
+        <h2>Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </>
   );
 };
 
