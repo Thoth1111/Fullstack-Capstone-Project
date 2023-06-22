49c8468c2ba564199275b8fd27b4996d1d95c74b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-22 10:50:38 +0300

    fix navbar error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,15 +57,7 @@ function NavigationPanel() {
           </Link>
         </li>
         <li className="mb-4 pr-16 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-
-          <Link to="/delete" className="">
-            Delete Vespa
-          </Link>
-        </li>
-        <li className="mb-4 pr-16 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-          <Link to="/" className="" onClick={handleClearSession}>
-            Sign Out
-
+          <Link to="/" className="" onClick={handleSignOut}>Sign Out</Link>
         </li>
       </ul>
       {deleteModalVisible && <DeleteModal onClose={handleCloseModal} />}
