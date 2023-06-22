f66cddbe142c15b17362a5a6c888558c4fb6953d
MosDevx <mosesk.wamae@gmail.com>
2023-06-22 11:19:28 +0300

    create reservation on API

--- src/pages/AddReservations.jsx
+++ src/pages/AddReservations.jsx
@@ -1,24 +1,19 @@
-import backimg from '../assets/background.jpg';
-import { useState,  } from 'react';
-
-
-import { useGetAllVespasQuery, useCreateReservationMutation } from "../redux/vespaAPI"
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import backimg from '../assets/background.jpg';
 
-
+import { useGetAllVespasQuery, useCreateReservationMutation } from '../redux/vespaAPI';
 
 function AddReservations() {
-  
-  const data  = ["vespa1", "vespa2", "vespa3", "vespa4", "vespa5", "vespa6"]
+  const data = ['vespa1', 'vespa2', 'vespa3', 'vespa4', 'vespa5', 'vespa6'];
 
   const userID = useSelector((state) => state.persistedReducer.id);
 
-  console.log(userID)
   const minDate = new Date().toISOString().slice(0, 10);
 
-  const [endDateMinDate, setEndDateMinDate] = useState(minDate)
+  const [endDateMinDate, setEndDateMinDate] = useState(minDate);
 
-  const [startDateMaxDate, setStartDateMaxDate] = useState(null)
+  const [startDateMaxDate, setStartDateMaxDate] = useState(null);
 
   const [selectedVespa, setSelectedVespa] = useState(data[0]);
   const [startDate, setStartDate] = useState(null);
@@ -28,36 +23,33 @@ function AddReservations() {
 
   const [createReservation, { isLoading: isCreating }] = useCreateReservationMutation();
 
-  
   const handleDropDownChange = (e) => {
     setSelectedVespa(e.target.value);
   };
 
   const handleStartDateChange = (e) => {
     setStartDate(e.target.value);
-    setEndDateMinDate(e.target.value)
+    setEndDateMinDate(e.target.value);
   };
 
   const handleEndDateChange = (e) => {
     setEndDate(e.target.value);
-    setStartDateMaxDate(e.target.value)
+    setStartDateMaxDate(e.target.value);
   };
 
-const handleSubmitClick = () => {
-  console.log(selectedVespa);
-  console.log(startDate);
-  console.log(endDate);
+  const handleSubmitClick = () => {
+    const reservation = {
+      reservation: {
+        user_id: userID,
+        room_id: 1,
+        start_date: startDate,
+        end_date: endDate,
+        description: 'I made a reservation FINALLLYY!!',
+      },
+    };
 
-  const reservation = {
-    user : "user1",
-    vespa: selectedVespa,
-    startDate: startDate,
-    endDate: endDate
-}
-
-
-}
-
+    createReservation(reservation);
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center gap-8 items-center text-white relative">
@@ -69,42 +61,48 @@ const handleSubmitClick = () => {
       <hr className="w-2/5 bg-gray-600 z-10" />
       <p className="tracking-widest z-10">
 
-          There are {data.length} Vespas available for rent. Please select the Vespa you want to rent, and the start and end date of your reservation
+        There are
+        {' '}
+        {data.length}
+        {' '}
+        Vespas available for rent. Please select the Vespa you want to rent, and the start and end date of your reservation
 
       </p>
-     
-     
+
       <div className="flex gap-4 z-10">
 
         <select id="countries" value={selectedVespa} onChange={handleDropDownChange} className="text-white-200 font-semibold  h-12 mt-7  px-4 rounded-full bg-transparent border-2 border-white">
 
           {data.map((item) => (
             <option value={item} key={item} className="text-black">{item}</option>
-          ))  
-          
-          }
+          ))}
 
- 
         </select>
-        <div className='flex-col items-center justify-center space-y-2 text-center'>
-        <p>Start Date:</p>
+        <div className="flex-col items-center justify-center space-y-2 text-center">
+          <p>Start Date:</p>
 
-        <input onChange={handleStartDateChange} type="date" id="start-date" min={minDate} 
-        max={startDateMaxDate}  name="start-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white"  />
+          <input
+            onChange={handleStartDateChange}
+            type="date"
+            id="start-date"
+            min={minDate}
+            max={startDateMaxDate}
+            name="start-date"
+            className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white"
+          />
         </div>
 
-        <div className='flex-col items-center justify-center space-y-2 text-center'>
-        <p>End Date:</p>
+        <div className="flex-col items-center justify-center space-y-2 text-center">
+          <p>End Date:</p>
 
-        <input onChange={handleEndDateChange} min={endDateMinDate} type="date" id="end-date" name="end-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white" />
+          <input onChange={handleEndDateChange} min={endDateMinDate} type="date" id="end-date" name="end-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white" />
         </div>
 
         <button type="submit" onClick={handleSubmitClick} className="bg-white font-semibold text-[#96bf01] py-2 h-12 mt-7 px-10 rounded-full">
           Book now
         </button>
       </div>
-   
-   
+
     </div>
   );
 }
