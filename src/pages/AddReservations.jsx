3941cdd324c0be11cf35ee08a7aae38ab06f3fe7
MosDevx <mosesk.wamae@gmail.com>
2023-06-22 10:07:02 +0300

    style form

--- src/pages/AddReservations.jsx
+++ src/pages/AddReservations.jsx
@@ -2,14 +2,15 @@ import backimg from '../assets/background.jpg';
 import { useState } from 'react';
 
 function AddReservations() {
+  
+  const data  = ["vespa1", "vespa2", "vespa3", "vespa4", "vespa5", "vespa6"]
 
-  const [selectedVespa, setselectedVespa] = useState(null);
+  const [selectedVespa, setselectedVespa] = useState(data[0]);
   const [startDate, setstartDate] = useState(null);
   const [endDate, setendDate] = useState(null);
 
   // const {data , error, isLoading} = useGetAllVespasQuery();
 
-  const data  = ["vespa1", "vespa2", "vespa3", "vespa4", "vespa5", "vespa6"]
   
   const handleDropDownChange = (e) => {
     setselectedVespa(e.target.value);
@@ -22,7 +23,12 @@ function AddReservations() {
   const handleEndDateChange = (e) => {
     setendDate(e.target.value);
   };
-  
+
+const handleSubmitClick = () => {
+  console.log(selectedVespa);
+  console.log(startDate);
+  console.log(endDate);
+}
 
 
   return (
@@ -31,13 +37,18 @@ function AddReservations() {
         <img src={backimg} alt="Background" className="h-full w-full object-fill " />
         <div className="absolute inset-0 z-0 opacity-90 bg-[#96bf01]" />
       </div>
-      <h1 className="font-bold tracking-widest text-3xl font-mono z-10">BOOk A Vespa</h1>
+      <h1 className="font-bold tracking-widest text-3xl font-mono z-10">Book A Vespa</h1>
       <hr className="w-2/5 bg-gray-600 z-10" />
-      <p className="tracking-widest z-10">oljloh.ohibiooooooooooooooooooooooooooooooo</p>
+      <p className="tracking-widest z-10">
+
+          There are {data.length} Vespas available for rent. Please select the Vespa you want to rent, and the start and end date of your reservation
+
+      </p>
      
      
-      <div className="flex gap-4 z-10">
-        <select id="countries" value={selectedVespa} onChange={handleDropDownChange} className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white">
+      <div className="flex gap-4 border-2 border-black z-10">
+
+        <select id="countries" value={selectedVespa} onChange={handleDropDownChange} className="text-white-200 font-semibold  h-12 mt-7  px-4 rounded-full bg-transparent border-2 border-white">
 
           {data.map((item) => (
             <option value={item} key={item} className="text-black">{item}</option>
@@ -47,11 +58,17 @@ function AddReservations() {
 
  
         </select>
-        <input type="date" id="start-date" name="start-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white" />
+        <div className='flex-col items-center justify-center space-y-2 text-center'>
+        <p>Start Date:</p>
+        <input onChange={handleStartDateChange} type="date" id="start-date" name="start-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white"  />
+        </div>
 
-        <input type="date" id="end-date" name="end-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white" />
+        <div className='flex-col items-center justify-center space-y-2 text-center'>
+        <p>End Date:</p>
+        <input onChange={handleEndDateChange} type="date" id="end-date" name="end-date" className="text-white-200 font-semibold py-2 px-4 rounded-full bg-transparent border-2 border-white" />
+        </div>
 
-        <button type="submit" className="bg-white font-semibold text-[#96bf01] py-2 px-10 rounded-full">
+        <button type="submit" onClick={handleSubmitClick} className="bg-white font-semibold text-[#96bf01] py-2 h-12 mt-7 px-10 rounded-full">
           Book now
         </button>
       </div>
