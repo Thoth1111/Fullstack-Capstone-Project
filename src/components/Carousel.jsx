ac4424445eed75812ef900553756fb0373e3c881
MosDevx <mosesk.wamae@gmail.com>
2023-06-23 16:53:07 +0300

    fix carousel display issue

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,4 @@
+import { data } from 'jquery';
 import React, { Component } from 'react';
 
 import Slider from 'react-slick';
@@ -7,19 +8,21 @@ import { Link } from 'react-router-dom';
 import Vespa from './Vespa';
 
 export default function Carousel() {
+  const { data: vespas, error, isLoading } = useGetAllVespasQuery();
+  
+  
+  
+  
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Oops! Something went wrong...</p>;
+  
+  let slidesToShow = vespas.length > 3 ? 3 : vespas.length;
   const settings = {
     infinite: false,
-    slidesToShow: 3,
+    slidesToShow: slidesToShow,
     slidesToScroll: 1,
     arrows: true,
   };
-
-  const { data: vespas, error, isLoading } = useGetAllVespasQuery();
-
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Oops! Something went wrong...</p>;
-
   return (
     <div className="h-5/6 flex">
     <Slider {...settings} className="flex items-center  my-auto h-5/6 w-full">
