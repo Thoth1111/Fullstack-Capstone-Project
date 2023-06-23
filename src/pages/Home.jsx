717e7164a19691758358cc397ca52139a568cb20
MosDevx <mosesk.wamae@gmail.com>
2023-06-23 03:14:59 +0300

    fix homepage width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
   return (
     <div>
    
-      <div className=" w-[1200px] ml-60 bg-slate-50 h-5/6 m-auto mt-24  text-center">
+      <div className=" w-[1100px] ml-60 bg-slate-50 h-5/6 m-auto mt-24  text-center">
 
         <p className="text-4xl mt-8 font-bold">Latest Models</p>
         <p className="mt-4 text-lg">Please Select A Vespa Model </p>
