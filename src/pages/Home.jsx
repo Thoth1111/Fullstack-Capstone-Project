fea0530592bf98ca468e02a0effa53274ecba3de
djo1975 <mladanilic1975@gmail.com>
2023-06-23 19:54:24 +0200

    Add base for Vespa details page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,9 @@ import Carousel from '../components/Carousel';
 function Home() {
   return (
     <div>
-
-      <div className=" w-[1100px] ml-60 bg-slate-50 h-5/6 m-auto mt-24  text-center">
-
+      <div className="w-[1100px] ml-60 bg-slate-50 h-5/6 m-auto mt-24 text-center">
         <p className="text-4xl mt-8 font-bold">Latest Models</p>
-        <p className="mt-4 text-lg">Please Select A Vespa Model </p>
+        <p className="mt-4 text-lg">Please Select A Vespa Model</p>
 
         <Carousel />
       </div>
