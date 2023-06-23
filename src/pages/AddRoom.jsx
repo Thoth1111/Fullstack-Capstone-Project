74b03500ad04302b9938be782f34d13d65ca5154
MosDevx <mosesk.wamae@gmail.com>
2023-06-23 20:47:27 +0300

    fix add vespa form

--- src/pages/AddRoom.jsx
+++ src/pages/AddRoom.jsx
@@ -79,7 +79,7 @@ function AddRoom() {
         </div>
 
         <div className="relative z-0 w-full mb-6 group">
-          <input type="text" value={vespaData.Description} name="Description" id="Description" className="block py-2.5 px-0 w-full  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={handleInputChange} />
+          <input type="text" value={vespaData.Description} name="Description" id="Description" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={handleInputChange} />
           <label htmlFor="Description" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
         </div>
 
