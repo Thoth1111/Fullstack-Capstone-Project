2a44f722ab6370a9a3fecb426bcc6cd1e1f1c7c0
djo1975 <mladanilic1975@gmail.com>
2023-06-24 07:34:13 +0200

    Conect vespa and home with redux

--- src/components/Vespa.jsx
+++ src/components/Vespa.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import getPallete from '../helpers/pallete';
 
 function Vespa({ icon: url, name, description }) {
   const [pallete, setPallete] = useState();
+  const token = useSelector((state) => state.persistedReducer.token); // Pridruživanje tokena iz Redux-a
+
   useEffect(() => {
     const vibrant = getPallete(url);
     vibrant.then((pallete) => setPallete(pallete));
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Carousel from '../components/Carousel';
 
 function Home() {
+  const token = useSelector((state) => state.persistedReducer.token);
+
   return (
     <div>
       <div className="w-[1100px] ml-60 bg-slate-50 h-5/6 m-auto mt-24 text-center">
