6d0a496be10155ff918f8abbbdda5051cd206537
MosDevx <mosesk.wamae@gmail.com>
2023-06-24 13:54:24 +0300

    finish styling details page responsiveness

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,36 +1,55 @@
-import CommentsList from "../components/CommentsList";
-import NewCommentForm from "../components/NewCommentForm";
+import CommentsList from '../components/CommentsList';
+import NewCommentForm from '../components/NewCommentForm';
 
 function Details() {
   return (
-    <div className="flex flex-col md:grid md:grid-cols-8 bg-yellow-100 ml-52 h-screen">
+    <div className="flex relative flex-col md:grid md:grid-cols-8  ml-52 h-screen">
 
-        <div className = " md:col-span-5 lg:col-span-6 flex items-center justify-center bg-red-200">
+      <div className=" md:col-span-5 lg:col-span-6 flex items-center justify-center">
 
-          <img src="https://as1.ftcdn.net/v2/jpg/04/88/07/66/1000_F_488076617_IVBsEeXAy56swgxUxXjDG3wKRTHJj2HR.jpg" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/04/88/07/66/1000_F_488076617_IVBsEeXAy56swgxUxXjDG3wKRTHJj2HR.jpg" alt="" />
+
+        <button className="absolute flex justify-end items-center left-0 top-4 md:top-auto md:bottom-16 w-16 md:w-24 rounded-tr-3xl rounded-br-3xl h-10 md:h-16 bg-lime-500 ">
+
+          <svg className="h-6 w-6 md:h-10 md:w-10 mr-2 md:mr-4 text-white" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            {' '}
+            <path stroke="none" d="M0 0h24v24H0z" />
+            {' '}
+            <path d="M18 15l-6-6l-6 6h12" transform="rotate(270 12 12)" />
+          </svg>
+
+        </button>
+
+      </div>
+
+      <div className="  md:mx-0 p-2 md:col-span-3 lg:col-span-2 flex flex-col-reverse md:flex-col ">
+
+        <div className="flex flex-col w-full items-center">
+          <p className="text-2xl font-bold"> VESPA NAME </p>
+          {/* <small className="md:text-md">$350 deposit upon any Vespa Purchase</small> */}
+
+          <NewCommentForm />
+
+          <CommentsList />
 
         </div>
 
-        <div className="  md:mx-0 p-2 md:col-span-3 lg:col-span-2 flex flex-col bg-green-200">
+        <button className="w-40  self-center mt-8 justify-between text-center mb-4 items-center rounded-2xl flex text-lg text-white border-none outline-none py-4 bg-lime-500 h-12">
 
-          <div className="flex flex-col w-full bg-orange-500 items-center">
-              <p className="text-2xl font-bold"> VESPA NAME </p>
-              {/* <small className="md:text-md">$350 deposit upon any Vespa Purchase</small> */}
-              
-              <NewCommentForm />
+          <p className="ml-10 font-bold text-lg">Reserve</p>
 
-              <CommentsList />
+          <svg className="h-6 w-6    text-white mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            {' '}
+            <circle cx="12" cy="12" r="10" />
+            {' '}
+            <polyline points="12 16 16 12 12 8" />
+            {' '}
+            <line x1="8" y1="12" x2="16" y2="12" />
+          </svg>
 
-              <button className="w-40  justify-between text-center items-center rounded-2xl flex text-lg text-white border-none outline-none py-4 bg-lime-500 h-10">
+        </button>
 
-                  <p className="ml-10">Reserve</p>
-              
-                  <svg class="h-6 w-6    text-white mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
-
-              </button>
-          </div>
-
-        </div>
+      </div>
     </div>
   );
 }
