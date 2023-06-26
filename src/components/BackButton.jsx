c6616e1c138914dfb5b3ba1d707243065fba0c09
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-26 08:49:29 +0300

    Adjust back button size for mobile

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -9,8 +9,15 @@ function BackButton() {
 
   return (
     <button className="absolute top-0 left-0 z-10 m-4 " onClick={handleGoBack}>
-
-      <svg className="w-20 h-20 text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <svg
+        className="w-20 h-20 text-white"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
         {' '}
         <circle cx="12" cy="12" r="10" /> <polyline points="12 8 8 12 12 16" />{' '}
         <line x1="16" y1="12" x2="8" y2="12" />
