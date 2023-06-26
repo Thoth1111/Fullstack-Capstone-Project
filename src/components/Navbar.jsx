ecb90f5ade0c4d1e218f0d350b92ac0cbc0e67b9
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-26 09:07:32 +0300

    Modify navbar to better match the design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -72,7 +72,7 @@ function NavigationPanel() {
           {/* Hamburger icon */}
           {!showNavMenu && (
             <button onClick={handleNavbar} className="bg-gray-200 md:hidden lg:hidden rounded-full p-2 ml-2">
-              <svg className="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg className="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
@@ -80,8 +80,8 @@ function NavigationPanel() {
 
           {/* Close icon */}
           {showNavMenu && (
-            <button onClick={handleNavbar} className="bg-gray-200 md:hidden lg:hidden rounded-full p-2 ml-2">
-              <svg className="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <button onClick={handleNavbar} className="bg-gray-200 md:hidden lg:hidden rounded-full p-2 ml-1">
+              <svg className="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
@@ -92,55 +92,55 @@ function NavigationPanel() {
             <ul className="flex flex-col pl-4">
               <li className="mb-4 top-0 ml-2">
                 <Link to="/">
-                  <img src={logo} alt="logo" className="w-40 h-35 mb-10 rounded-lg" />
+                  <img src={logo} alt="logo" className=" absolute top-6 left-9 w-30 h-20 mb-10 rounded-lg" />
                 </Link>
               </li>
-              <li className="mb-4 pr-14 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-                <Link to="/addvespa" className="">
-                  Add Vespa
+              <li className="mb-1 pr-14 py-2 pl-2 text-gray-700 hover:bg-[#a3c837] hover:text-white">
+                <Link to="/addvespa" className="text-lg">
+                  ADD VESPA
                 </Link>
               </li>
-              <li className="mb-4 pr-14 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-                <Link to="/myreservations" className="">
-                  My Reservation
+              <li className="mb-1 pr-14 py-2 pl-2 text-gray-700 hover:bg-[#a3c837] hover:text-white">
+                <Link to="/myreservations" className="text-lg">
+                  MY RESERVATIONS
                 </Link>
               </li>
-              <li className="mb-4 pr-16 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-                <button className="" onClick={handleDelete}>
-                  Delete Vespa
+              <li className="mb-1 pr-16 py-2 pl-2 text-gray-700 hover:bg-[#a3c837] hover:text-white">
+                <button className="text-lg" onClick={handleDelete}>
+                  DELETE VESPA
                 </button>
               </li>
-              <li className="mb-4 pr-16 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-                <Link to="/reserve" className="">
-                  Reserve
+              <li className="mb-1 pr-16 py-2 pl-2 text-gray-700 hover:bg-[#a3c837] hover:text-white">
+                <Link to="/reserve" className="text-lg">
+                  RESERVE
                 </Link>
               </li>
-              <li className="mb-4 pr-16 py-2 pl-2 hover:bg-[#a3c837] hover:text-white">
-                <Link to="/" className="" onClick={handleSignOut}>
-                  Sign Out
+              <li className="mb-1 pr-16 py-2 pl-2 text-gray-700 hover:bg-[#a3c837] hover:text-white">
+                <Link to="/" className="text-lg" onClick={handleSignOut}>
+                  SIGN OUT
                 </Link>
               </li>
             </ul>
             {deleteModalVisible && <DeleteModal className="w-full" onClose={handleCloseModal} />}
-            <div className="pb-8 ml-4">
-              <div className="flex items-center w-full align-middle mb-8 gap-x-2">
+            <div className="pb-8 mx-auto">
+              <div className="flex items-center w-full align-middle mb-2 gap-x-2">
                 <a href="#">
-                  <img src={icon1} alt="logo" className="w-7 h-7 rounded-full" />
+                  <img src={icon1} alt="logo" className="w-5 h-5 rounded-full" />
                 </a>
                 <a href="#">
-                  <img src={icon2} alt="logo" className="w-7 h-7 rounded-full" />
+                  <img src={icon2} alt="logo" className="w-5 h-5 rounded-full" />
                 </a>
                 <a href="#">
-                  <img src={icon3} alt="logo" className="w-7 h-7" />
+                  <img src={icon3} alt="logo" className="w-5 h-5" />
                 </a>
                 <a href="#">
-                  <img src={icon4} alt="logo" className="w-7 h-7 rounded-full" />
+                  <img src={icon4} alt="logo" className="w-5 h-5 rounded-full" />
                 </a>
                 <a href="#">
-                  <img src={icon5} alt="logo" className="w-7 h-7 rounded-full" />
+                  <img src={icon5} alt="logo" className="w-5 h-5 rounded-full text-gray-700" />
                 </a>
               </div>
-              <p className="font-light text-gray-500">@2023</p>
+              <p className="font-light text-gray-500 text-base mx-auto text-center">@2023</p>
             </div>
           </>
         )}
