d929bf609a6f47ddc3040ceb8e32941f9b75a7b3
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-27 13:11:07 +0300

    Complete AddReservation page tests

---
+++ src/_tests_/AddReservations.test.js
@@ -0,0 +1,34 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; 
+import 'mock-local-storage';
+import { MemoryRouter } from 'react-router-dom';
+import { store } from '../redux/store';
+import { Provider } from 'react-redux';
+import AddReservations from '../pages/AddReservations';
+
+describe('AddReservations', () => {
+    test('renders AddReservations component', async () => {
+        sessionStorage.setItem('token', 'uSeRtOkEn');
+
+        render(
+        <Provider store={store}>
+            <MemoryRouter>
+                <AddReservations />
+            </MemoryRouter>
+        </Provider>
+        );
+
+        try {
+            await waitFor(() => {
+                return !screen.getByText('Loading...');
+            });
+
+            expect(screen.queryByText('Book A Vespa')).toBeInTheDocument();
+            expect(screen.getByText('Start Date')).toBeInTheDocument();
+            expect(screen.getByText('End Date')).toBeInTheDocument();
+            expect(screen.getByPlaceholderText('Enter a description of the reservation')).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: 'Book now' })).toBeInTheDocument();
+        } catch (error) {
+        }       
+    });
+});
