ea35af334a33c847c2d284965f8e16e4447e18fd
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-29 09:54:21 +0300

    Add loading and error handling for login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,6 +8,8 @@ import { encryptToken } from '../helpers/encryption';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -15,6 +17,9 @@ const Login = () => {
     e.preventDefault();
 
     try {
+      setLoading(true);
+      setError('');
+
       const { user, token } = await apiRequests.login(email, password);
       const { username, id } = user;
       dispatch(setAuthInfo({ username, id, token }));
@@ -26,16 +31,30 @@ const Login = () => {
       navigate('/home');
     } catch (error) {
       console.error(error);
+      setError('Incorrect email or password. Please try again.');
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <>
+      {loading ? (
+        <div className="flex min-h-screen bg-[#FFB400] flex-1 justify-center items-center">
+          <div className="flex flex-col h-1/3 w-1/3 p-6 items-center bg-white rounded-lg">
+            <span className="animate-spin rounded-full h-10 w-10 mb-8 border-t-2 border-b-2 border-l-4 border-lime-500"></span>
+            <p className="font-bold">Signing in...</p>
+          </div>
+        </div>
+      ) : (
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log In</h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          {error && (
+            <p className="text-red-500 text-center mb-4 mx-auto">{error}</p>
+          )}
           <form onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
@@ -69,6 +88,7 @@ const Login = () => {
           </form>
         </div>
       </div>
+      )}
     </>
   );
 }
